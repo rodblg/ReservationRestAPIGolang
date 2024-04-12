4b09d6f9ed961aeaa7af1069561298ca4050fb2b
rodblg <rodrigoblancas19@gmail.com>
2024-04-12 15:26:39 -0600

    add Makefile to build, run and test scripts. Add bin to .gitignore and download chi to go.sum and go.mod

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rodblg/ReservationRestAPIGolang
 
 go 1.20
+
+require github.com/go-chi/chi/v5 v5.0.12 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	})
+
+	http.ListenAndServe(":8080", r)
 }
